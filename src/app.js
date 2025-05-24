6dcd1bfb34d7d73946c18069eb0cfe5e527aa793
Luis Arce <luis.arce22@gmail.com>
2025-05-23 21:50:21 -0400

    Adding global error handler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const patientRoutes = require('./routes/patientRoutes') 
+const errorHandler = require('./middleware/errorHandler')
+
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/patients', patientRoutes)
+
+app.use(errorHandler)
 
 module.exports = app
